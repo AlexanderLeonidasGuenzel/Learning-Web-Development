72caea7b154962cfccdc49791298bdc38f904a39
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-02 00:08:45 +0100

    add: slider

--- scripts/meme-generator.js
+++ scripts/meme-generator.js
@@ -2,6 +2,10 @@ const canvas = document.getElementById("memeCanvas");
 const ctx = canvas.getContext("2d");
 const imageInput = document.getElementById("imageInput");
 let uploadedImage = null;
+const widthSlider = document.getElementById("widthSlider");
+const heightSlider = document.getElementById("heightSlider");
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
 
 imageInput.addEventListener("change", (event) => {
   const file = event.target.files[0];
@@ -19,6 +23,17 @@ imageInput.addEventListener("change", (event) => {
   reader.readAsDataURL(file);
 });
 
+widthSlider.addEventListener("input", resizeCanvas);
+heightSlider.addEventListener("input", resizeCanvas);
+widthSlider.addEventListener("input", () => {
+  canvasWidth = widthSlider.value;
+  resizeCanvas();
+});
+heightSlider.addEventListener("input", () => {
+  canvasHeight = heightSlider.value;
+  resizeCanvas();
+});
+
 function drawImage() {
   if (uploadedImage) {
     // Clear canvas and set canvas dimensions to fit the image
@@ -30,19 +45,35 @@ function drawImage() {
     const bottomText = document.getElementById("bottomText").value;
 
     // Set text styles
-    ctx.font = "30px Impact";
+    ctx.font = "40px Impact";
     ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 1;
     ctx.textAlign = "center";
 
     // Draw top text
-    ctx.fillText(topText, canvas.width / 2, 50);
-    ctx.strokeText(topText, canvas.width / 2, 50);
+    ctx.fillText(topText, canvas.width / 2, 70);
+    ctx.strokeText(topText, canvas.width / 2, 70);
 
     // Draw bottom text
-    ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-    ctx.strokeText(bottomText, canvas.width / 2, canvas.height - 20);
+    ctx.fillText(bottomText, canvas.width / 2, canvas.height - 50);
+    ctx.strokeText(bottomText, canvas.width / 2, canvas.height - 50);
+  }
+}
+
+function resizeCanvas() {
+  canvasWidth = widthSlider.value;
+  canvasHeight = heightSlider.value;
+
+  canvas.width = canvasWidth; // Breite des Canvas aktualisieren
+  canvas.height = canvasHeight; // Höhe des Canvas aktualisieren
+
+  if (widthSlider.value <= 600 && heightSlider.value <= 500) {
+    canvas.width = widthSlider.value;
+    canvas.height = heightSlider.value;
+    drawImage();
+  } else {
+    alert("No no!");
   }
 }
 
@@ -51,8 +82,12 @@ function generateMeme() {
 }
 
 function downloadMeme() {
-  const link = document.createElement("a");
-  link.download = "meme.png";
-  link.href = canvas.toDataURL();
-  link.click();
+  if (uploadedImage) {
+    const link = document.createElement("a");
+    link.download = "meme.png";
+    link.href = canvas.toDataURL();
+    link.click();
+  } else {
+    alert("No Way!");
+  }
 }
