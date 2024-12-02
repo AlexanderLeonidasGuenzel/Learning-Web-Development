8a3a65a2a65d3e2070928b1dee7464078fca85a9
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-02 22:57:10 +0100

    add: eventlistener to cards

--- scripts/memory.js
+++ scripts/memory.js
@@ -1,1 +1,7 @@
+const cards = document.querySelectorAll(".card");
 
+cards.forEach((card) => {
+  card.addEventListener("click", function (event) {
+    console.log(`Die Karte mit der ID "${event.target.id}" wurde angeklickt.`);
+  });
+});
