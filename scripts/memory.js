315de2be23cfb3210a5d4002123bbc68de24fbbd
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-04 03:05:54 +0100

    add: card flip

--- scripts/memory.js
+++ scripts/memory.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
-  card.addEventListener("click", function (event) {
-    console.log(`Die Karte mit der ID "${event.target.id}" wurde angeklickt.`);
+  card.addEventListener("click", () => {
+    card.classList.toggle("flip");
   });
 });
