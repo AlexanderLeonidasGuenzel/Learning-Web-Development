e32c6071754a1ca400159f20f5741ffa88909181
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-05 23:54:55 +0100

    add: isFlipped

--- scripts/memory.js
+++ scripts/memory.js
@@ -1,4 +1,6 @@
 const cards = document.querySelectorAll(".card");
+let isFlipped = false;
+let firstSide, secondSide;
 
 cards.forEach((card) => {
   card.addEventListener("click", flip);
@@ -6,4 +8,14 @@ cards.forEach((card) => {
 
 function flip() {
   this.classList.toggle("flip");
+
+  if (!isFlipped) {
+    isFlipped = true;
+    secondSide = this;
+    console.log({ isFlipped, secondSide });
+  } else {
+    isFlipped = false;
+    firstSide = this;
+    console.log({ isFlipped, firstSide });
+  }
 }
