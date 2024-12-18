a682d5cf8b22224ff27c131408a59f49ead02fcb
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-18 14:10:00 +0100

    add: calculator shop clicked number and clear display

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,26 @@
 console.log("cal loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach((button) => button.addEventListener("click", behaviour));
+});
+
+function behaviour() {
+  console.log("Click!" + this.innerHTML);
+  if (this.classList.contains("number")) {
+    display(this.innerHTML);
+  } else if (this.classList.contains("clear")) {
+    clear();
+  }
+}
+
+function display(number) {
+  const displayElement = document.querySelector(".display");
+  displayElement.value = displayElement.value + number;
+}
+
+function clear() {
+  console.log("clear");
+  const displayElement = document.querySelector(".display");
+  displayElement.value = "";
+}
