ed7ca8861868bcca47b36909494e0aa120f373ca
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-20 06:07:53 +0100

    add: planet

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
   buttons.forEach((button) => button.addEventListener("click", behaviour));
 });
 
+let memory = 0;
+let operator = "";
+
 function behaviour() {
   if (this.classList.contains("number")) {
     showDigit(this.innerHTML);
@@ -16,13 +19,30 @@ function behaviour() {
     stepBack();
   } else if (this.classList.contains("plus")) {
     showPlusSymbol();
+    putIntoMemory();
+  }
+}
+
+function putIntoMemory() {
+  const displayElement = document.querySelector(".display");
+  console.log(displayElement.value);
+  const length = displayElement.value.length;
+  const operator = displayElement.value.charAt(length - 1);
+  console.log(operator);
+  const numberValue = displayElement.value.slice(-1, 0);
+  console.log(displayElement.value);
+  if (numberValue.includes(".")) {
+    console.log("float");
+  } else {
+    console.log("integer");
+    memory = Number.parseInt(numberValue);
   }
 }
 
 function showDigit(number) {
   // console.log(Number.parseInt(number));
   const displayElement = document.querySelector(".display");
-  const maxLength = 17;
+  const maxLength = 13;
   const lengthWhithoutWhiteSpaces = displayElement.value.replace(
     /\s+/g,
     ""
@@ -30,9 +50,7 @@ function showDigit(number) {
 
   if (lengthWhithoutWhiteSpaces < maxLength) {
     if (!number.includes("1")) {
-      if (!(displayElement.value.length === 0 && number === "0")) {
-        displayElement.value = displayElement.value + number;
-      }
+      displayElement.value = displayElement.value + number;
     } else {
       displayElement.value = displayElement.value + " " + number;
     }
@@ -41,15 +59,14 @@ function showDigit(number) {
 }
 
 function showPlusSymbol() {
-  console.log("plus");
   const displayElement = document.querySelector(".display");
   const textLength = displayElement.value.length;
   if (
     textLength &&
     !displayElement.value.charAt(textLength - 1).includes("+")
   ) {
-    console.log("+");
     displayElement.value = displayElement.value + "+";
+    console.log("plus");
   }
 }
 
