aaddd374b276e91b0d9d4ec34c04f33bb504e4b0
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2024-12-22 21:06:43 +0100

    change: design and script

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,99 +1,100 @@
-console.log("cal loaded");
+const displayElement = document.querySelector(".display");
+let number = "";
+let operator = "";
 
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".button");
   buttons.forEach((button) => button.addEventListener("click", behaviour));
 });
 
-let memory = 0;
-let operator = "";
-
 function behaviour() {
-  if (this.classList.contains("number")) {
-    showDigit(this.innerHTML);
-  } else if (this.classList.contains("point")) {
-    point(this.innerHTML);
-  } else if (this.classList.contains("clear")) {
-    clear();
-  } else if (this.classList.contains("stepBack")) {
-    stepBack();
-  } else if (this.classList.contains("plus")) {
-    showPlusSymbol();
-    putIntoMemory();
-  }
-}
-
-function putIntoMemory() {
-  const displayElement = document.querySelector(".display");
-  console.log(displayElement.value);
-  const length = displayElement.value.length;
-  const operator = displayElement.value.charAt(length - 1);
-  console.log(operator);
-  const numberValue = displayElement.value.slice(-1, 0);
-  console.log(displayElement.value);
-  if (numberValue.includes(".")) {
-    console.log("float");
-  } else {
-    console.log("integer");
-    memory = Number.parseInt(numberValue);
-  }
-}
-
-function showDigit(number) {
-  // console.log(Number.parseInt(number));
-  const displayElement = document.querySelector(".display");
   const maxLength = 14;
-  const lengthWhithoutWhiteSpaces = displayElement.value.replace(
-    /\s+/g,
-    ""
-  ).length;
 
-  if (lengthWhithoutWhiteSpaces < maxLength) {
-    if (!number.includes("1")) {
-      displayElement.value = displayElement.value + number;
-    } else {
-      displayElement.value = displayElement.value + " " + number;
+  if (this.classList.contains("clear")) {
+    clear();
+    return 0;
+  }
+  if (this.classList.contains("stepBack")) {
+    stepBack();
+    return 0;
+  } else if (displayElement.value.length < maxLength) {
+    if (this.classList.contains("digit")) {
+      let a = this.innerHTML;
+      clearDisplay();
+      putIntoMemory(a);
+      // showDigit(a);
+      showNumber();
+    } else if (this.classList.contains("point")) {
+      let a = this.innerHTML;
+      point(a);
+    } else if (this.classList.contains("operator")) {
+      let a = this.innerHTML;
+      clearDisplay();
+      showOperator(a);
     }
   }
-  console.log("length " + displayElement.value.replace(/\s+/g, "").length);
 }
 
-function showPlusSymbol() {
-  const displayElement = document.querySelector(".display");
-  const textLength = displayElement.value.length;
-  if (
-    textLength &&
-    !displayElement.value.charAt(textLength - 1).includes("+")
-  ) {
-    displayElement.value = displayElement.value + "+";
-    console.log("plus");
-  }
+// memory = memory + Number.parseInt(displayElement.value);
+function clearDisplay() {
+  displayElement.value = "";
+}
+
+function putIntoMemory(digit) {
+  number = number + digit;
+  console.log(number);
+}
+
+function showNumber() {
+  displayElement.value = getFromMemory();
+}
+
+function getFromMemory() {
+  return number;
+}
+
+// function showDigit(digit) {
+//   displayElement.value = displayElement.value + digit;
+// }
+
+function showOperator(operator) {
+  // const textLength = displayElement.value.length;
+  displayElement.value = displayElement.value + operator;
+  // if (
+  //   textLength &&
+  //   !displayElement.value.charAt(textLength - 1).includes("+")
+  // ) {
+
+  // }
 }
 
 function clear() {
-  console.log("clear");
-  const displayElement = document.querySelector(".display");
   displayElement.value = "";
+  number = "";
   console.clear();
 }
 
 function stepBack() {
-  console.log("back");
-  const displayElement = document.querySelector(".display");
   displayElement.value = displayElement.value.slice(0, -1);
-  console.log("length " + displayElement.value.length);
 }
 
-function point(point) {
-  console.log(".");
-  const displayElement = document.querySelector(".display");
-  const textLength = displayElement.value.length;
-  if (!textLength) {
-    displayElement.value = displayElement.value + "0" + point;
-  } else if (!displayElement.value.includes(".")) {
-    console.log("point");
-    displayElement.value = displayElement.value + point;
-    console.log("length " + displayElement.value.length);
-  }
-  console.log("length " + displayElement.value.length);
-}
+// function point(point) {
+//   const textLength = displayElement.value.length;
+
+//   if (!textLength) {
+//     displayElement.value = displayElement.value + "0" + point;
+//   } else if (!displayElement.value.includes(".")) {
+//     displayElement.value = displayElement.value + point;
+//   }
+// }
+
+// const lengthWhithoutWhiteSpaces = displayElement.value.replace(
+//   /\s+/g,
+//   ""
+// ).length;
+
+// if (numberValue.includes(".")) {
+//   memory = Number.parseFloat(numberValue);
+// } else {
+//   memory = Number.parseInt(numberValue);
+// }
