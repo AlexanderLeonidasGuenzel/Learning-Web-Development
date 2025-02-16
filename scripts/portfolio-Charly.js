400b18090431721908cf4ececdf884a633eef563
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2025-02-16 20:30:15 +0100

    change: home style

--- scripts/portfolio-Charly.js
+++ scripts/portfolio-Charly.js
@@ -21,30 +21,46 @@ function slowScrollTo(targetSelector, duration = 1000) {
   }
 
   function easeOutQuad(t) {
-    return t * (2 - t); // Langsamer am Ende
+    return t * (2 - t);
   }
 
   requestAnimationFrame(scrollAnimation);
 }
-const colors = ["w", "x", "y", "z", "a"];
-const persons = [
-  "Sarah Weber",
-  "Nico Fisher",
-  "Anne Tiger",
-  "Kim Wong",
-  "Bianca Keen",
-  "Thimo Zarvesk",
-];
+const colors = ["red", "yellow", "blue", "green", "orange"];
+const person = "Charly Portmann";
+const firstName = person.slice(0, person.indexOf(" "));
+const lastName = person.slice(person.indexOf(" ") + 1, person.length);
+console.log(firstName);
+console.log(lastName);
 
 window.onload = () => {
   setTimeout(() => {
     document.querySelector("#home")?.scrollIntoView({ behavior: "instant" });
   }, 0);
   const rN = Math.floor(Math.random() * colors.length);
-  const rP = Math.floor(Math.random() * persons.length);
-  const all = document.querySelectorAll(".portfolio");
-  all.forEach((e) => {
-    e.innerHTML = persons[rP];
-    e.classList.toggle(colors[rN]);
+  const devName = document.querySelectorAll(".developer-name");
+  devName.forEach((e) => {
+    let nameLetters = [];
+    nameLetters[0] = document.createElement("span");
+    nameLetters[0].textContent = firstName.charAt(0);
+    nameLetters[0].classList.add(colors[rN]);
+
+    nameLetters[1] = document.createElement("span");
+    nameLetters[1].textContent = firstName.slice(1);
+    nameLetters[1].classList.add("white");
+
+    nameLetters[2] = document.createElement("span");
+    nameLetters[2].textContent = " " + lastName.charAt(0);
+    nameLetters[2].classList.add(colors[rN]);
+
+    nameLetters[3] = document.createElement("span");
+    nameLetters[3].textContent = lastName.slice(1);
+    nameLetters[3].classList.add("white");
+
+    e.innerHTML = "";
+
+    for (let i = 0; i < nameLetters.length; i++) {
+      e.appendChild(nameLetters[i]);
+    }
   });
 };
